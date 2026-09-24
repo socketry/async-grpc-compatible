900709113704c04e4e80f3d2b58a4293026e794f
Samuel Williams <samuel.williams@shopify.com>
2026-09-24 13:56:17 +1200

    Use client response cleanup with ensure

--- gems.rb
+++ gems.rb
@@ -12,7 +12,7 @@ local_async_grpc_path = File.expand_path("../async-grpc", __dir__)
 if File.directory?(local_async_grpc_path)
 	gem "async-grpc", path: local_async_grpc_path
 else
-	gem "async-grpc", git: "https://github.com/socketry/async-grpc.git", ref: "5ab808b32bf9995b177039c5e15f1a911e2cd6e6"
+	gem "async-grpc", git: "https://github.com/socketry/async-grpc.git", ref: "dbb55a9fbb96fa7fc7584dc1a8b0ab33845d14d0"
 end
 
 group :maintenance, optional: true do
