9e0cfc6f531bdf12484efe29166655f2ddda3a80
Samuel Williams <samuel.williams@shopify.com>
2026-09-24 15:46:14 +1200

    Track native response error factory

--- gems.rb
+++ gems.rb
@@ -12,7 +12,7 @@ local_async_grpc_path = File.expand_path("../async-grpc", __dir__)
 if File.directory?(local_async_grpc_path)
 	gem "async-grpc", path: local_async_grpc_path
 else
-	gem "async-grpc", git: "https://github.com/socketry/async-grpc.git", ref: "c93039a9ef3abb9dac36993a9a117a5926c9f7f8"
+	gem "async-grpc", git: "https://github.com/socketry/async-grpc.git", ref: "90623d0037aa7d5ae109cc79f72a57620e663577"
 end
 
 group :maintenance, optional: true do
